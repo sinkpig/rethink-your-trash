dedefa941d86d159cbd2efabfbcee3eefae72e11
Milo Ramirez <sinkpig00@gmail.com>
2022-11-12 11:45:23 -0300

    Model, and DB for user and new rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatabl
+
+  validates :name, :address, :role, presence: true
+  validates :description, presence: true, if: :cooperative?
+
+  enum role: {
+    person: "person",
+    cooperative: "cooperative"
+  }
+
+  def cooperative?
+    role == "cooperative"
+  end
 end
