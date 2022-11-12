cf1dee67cd16eee4013876d1b0e18864184a3335
tanaka <patricia.tanaka.3@hotmail.com>
2022-11-12 17:11:05 -0300

    updated view for trades

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,9 +1,5 @@
 class TradesController < ApplicationController
 
-  def index
-    @trades = Trade.where(user_id: current_user.id)
-  end
-
   def new
     @trade = Trade.new
   end
@@ -11,8 +7,8 @@ class TradesController < ApplicationController
   def create
     @trade = Trade.new(trade_params)
 
-    if @trade.save
-      redirect_to trades_path(@trade)
+    if @trade.save!
+      redirect_to root
     else
       render :new, status: :unprocessable_entity
       #the render shows the fields that must be corrected by the user
@@ -25,16 +21,20 @@ class TradesController < ApplicationController
 
   def update
     if @trade.update(trade_params)
-      redirect_to trades_path(@trade)
+      redirect_to root
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @trade = Trade.find(params[:id])
+  end
+
   def destroy
     trade = Trade.find(params[:id])
     trade.destroy
-    redirect_to trades_path(trade), status: :see_other
+    redirect_to root, status: :see_other
   end
 
   private
