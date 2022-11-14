5e364ddaf3df9cc6018fabd8dc61d9cae990708c
Daniela Rios <danirioscampelo@gmail.com>
2022-11-14 15:26:20 -0300

    metodo home adicionado no pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @trades = current_user.trades.where(status: "In progress" || "Pending")
+    @materials = Material.all
+    #map with cooperatives
+    #wallet
   end
 end
