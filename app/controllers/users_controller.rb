21c8a2507c84f45a8566d93ddf0389cb2de85ab8
tanaka <patricia.tanaka.3@hotmail.com>
2022-11-19 16:40:52 -0300

    deleted the user controller and views

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    @cooperatives = User.where(role: "cooperative")
-  end
-
-  def show
-    @cooperative = User.find(params[:id])
-  end
-end
