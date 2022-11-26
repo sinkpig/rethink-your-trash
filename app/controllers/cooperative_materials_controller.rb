c91d1953ebe442339b3c002aa2e3f9dc7d22a194
Daniela Rios <danirioscampelo@gmail.com>
2022-11-26 18:26:01 -0300

    cooperative_materials model controller e table created and working - view still need to be set

--- app/controllers/cooperative_materials_controller.rb
+++ app/controllers/cooperative_materials_controller.rb
@@ -1,14 +1,23 @@
 class CooperativeMaterialsController < ApplicationController
+  before_action :set_cooperative, only: %i[index new]
+
 
   def index
-    @cooperative_materials = CooperativeMaterial.where(cooperative_id: current_user.id)
+    @cooperative_materials = CooperativeMaterial.where(user_id: @cooperative.id)
+  end
+
+  def new
+    @cooperative_material = CooperativeMaterial.new
+    @cooperative_materials = CooperativeMaterial.where(user_id: @cooperative.id)
+    @materials = Material.where.not(id: @cooperative_materials.map(&:material_id))
+      # eh uma lista de todos os materiais que ainda nao estao listados nessa cooperative (vai mostrart apenas os que faltam como opcao para serem selecionados)
   end
 
   def create
     @cooperative_material = CooperativeMaterial.new(cooperative_material_params)
     @cooperative_material.user = current_user
-    if current_user.cooperative? && @cooperative_material.save?
-      redirect_to root_path
+    if current_user.cooperative? && @cooperative_material.save
+      redirect_to cooperative_cooperative_materials_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,6 +37,10 @@ class CooperativeMaterialsController < ApplicationController
 
   def cooperative_material_params
     params.require(:cooperative_material).permit(:material_id)
+  end
+
+  def set_cooperative
+    @cooperative = User.find(params[:cooperative_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
   devise_for :users
   root to: "pages#home"
   get "/profile", to: "pages#profile"
