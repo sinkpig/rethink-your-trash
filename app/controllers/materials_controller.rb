5443ad1146873be17fe197b4fc2b6c8142963114
Milo Ramirez <sinkpig00@gmail.com>
2022-12-01 20:32:47 -0300

    fixed various things

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,5 +1,7 @@
 class MaterialsController < ApplicationController
   def index
     @materials = Material.all
+    # Make relations between materials and cooperatives
+    # @cooperative
   end
 end
