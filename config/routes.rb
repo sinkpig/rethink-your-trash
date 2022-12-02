e2733340c5e65a3bff451d75aa6e9dbda4cf3091
Daniela Rios <danirioscampelo@gmail.com>
2022-12-01 22:03:02 -0300

    lista de materials da cooperative aparecendo no profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   # get 'users/show'
   devise_for :users
   root to: "pages#home"
-  get "/profile", to: "pages#profile"
+
+  scope "profile" do
+    get "", to: "pages#profile", as: "profile"
+  end
+  # delete "cooperative_material/:id", to: "cooperative_materials#destroy", as: "cooperative_material_delete"
 
   resources :cooperatives, only: %i[index show] do
     resources :trades, only: %i[new create]
-    resources :cooperative_materials, only: %i[index new create destroy]
+    resources :cooperative_materials, only: %i[index new create]
   end
   resources :trades, only: %i[destroy update]
   resources :materials, only: %i[index]
-  # resources :cooperative_materials, only: %i[destroy]
+  resources :cooperative_materials, only: %i[destroy]
 end
